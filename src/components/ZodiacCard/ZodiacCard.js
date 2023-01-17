c4a83500885b2451202ed8cec880ab5dfeeb4039
Akilah Powell <atpjun@gmail.com>
2023-01-17 08:58:58 -0800

    hopefully everything passes

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -5,7 +5,7 @@ import '../../data.js';
 export default function ZodiacCard({ name, dates, symbol }) {
   return (
     <div className="zodiac-card">
-      <img alt={name} src={`../Signs/${name}.png`} />
+      <img alt={name} src={`./public/${name}.png`} />
       <span className="name">{name}</span>
       <span>{dates}</span>
       <span>{symbol}</span>
