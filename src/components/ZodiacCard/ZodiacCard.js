5ddd2aacbc30634f86a21660798efde390a9f8e1
Akilah Powell <atpjun@gmail.com>
2023-01-27 15:54:53 -0800

    changed files and accidentally had duplicate zodiaccard. js

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,11 +1,13 @@
 
 import './ZodiacCard.css';
 import '../../data.js';
+import zodiacImg from '../Images';
+
 
 export default function ZodiacCard({ name, dates, symbol }) {
   return (
     <div className="zodiac-card">
-      <img alt={name} src={`./public/${name}.png`} />
+      <img alt={name} src={`${zodiacImg.name}.png`} />
       <span className="name">{name}</span>
       <span>{dates}</span>
       <span>{symbol}</span>
